492a19225478d535a7241b07979392315b4dcd44
keep1tstupid <alexandra.globa@gmail.com>
2021-02-15 20:51:02 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Todolist from "./Todolist";
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Todolist />
   </React.StrictMode>,
   document.getElementById('root')
 );
